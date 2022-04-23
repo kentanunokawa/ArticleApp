d6f7afc206ddedaa6468ba00a62447ad80f830cc
布川健大 <81751512+kentanunokawa@users.noreply.github.com>
2022-04-23 13:10:41 +0900

    first commit ~46P

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
+<style>
+    body {font-size:16pt; color:#999;}
+    h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+</style>
+EOF;
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag,$txt){
+    return "<{$tag}>".$txt."<{/$tag}>";
+}
+
+class HelloController extends Controller
+{
+    public function index(){
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title','Hello/index').$style.$body.tag('h1','index').tag('p','this is index page').'<a href="/hello/other">go to ather page</a>'.$end;
+        return $html;
+    }
+
+    public function other(){
+        global $head, $style, $body, $end;
+
+        
+    }
+}
